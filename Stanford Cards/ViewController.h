b5f145606884470c1c3b7b736073265ae0ef9b4b
Rafael Pereira <rafaelbsides@gmail.com>
2013-11-02 05:52:55 -0200

    Initial Commit

---
+++ Stanford Cards/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Stanford Cards
+//
+//  Created by Rafael Pereira on 02/11/13.
+//  Copyright (c) 2013 BSIDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Stanford Cards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stanford Cards
+//
+//  Created by Rafael Pereira on 02/11/13.
+//  Copyright (c) 2013 BSIDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
